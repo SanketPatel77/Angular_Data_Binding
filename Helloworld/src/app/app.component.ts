c03405217244981bb1af1b8914bc9b64e4adf414
SanketPatel77 <sanketpatel4872@gmail.com>
2025-03-06 15:48:41 +0530

    [Sanket] Add . To display Hello from BridgeLabz with a user inputted name.

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, FormsModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello World';
-  imageUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM"
-        alt="BridgeLabz logo"
-  url = "http://localhost:4200/"
-  ngOnInit(): void{
-    this.title = "Hello from BridgeLabz";
-  }
+  imageUrl = "assets/BL_logo_square_png.png";
+  url = "http://localhost:4200/";
+  userName: string = "";
 
-  onClick($event: any){
+  onClick($event: any) {
     console.log("Button is clicked", $event);
-    window.open(this.url,"_blank");
+    window.open(this.url, "_blank");
   }
 }
